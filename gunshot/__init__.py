a0b5f30cf991e0901220803ca6aebf0133eeeef8
Brijesh Krishna <63464137+Brijeshkrishna@users.noreply.github.com>
2021-09-12 06:27:23 -0800

    Delete gunshot directory

--- gunshot/__init__.py
+++
@@ -1,1 +0,0 @@
-from .gunshot import gun
--- gunshot/core/errors.py
+++
@@ -1,8 +0,0 @@
-class lookupError(Exception):
-    def __init__(self):
-        super(Exception, self).__init__("It is not a IPV4 or IPv6 address")
-
-
-class tinyUrlError(Exception):
-    def __init__(self):
-        super(Exception, self).__init__("Unable to shorten the URL ")
